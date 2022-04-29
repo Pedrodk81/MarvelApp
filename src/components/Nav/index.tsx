eba0a6dc9fce20cc44ff613ccc86c6dbc1504116
Pedrodk81 <81542805+Pedrodk81@users.noreply.github.com>
2022-04-29 12:25:05 -0300

    navigation bar adjustments

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import { ReactElement } from "react";
 import { Link, useLocation, Navigate } from "react-router-dom";
 import { useDispatch } from 'react-redux'
 
 import { DESTROY_SESSION } from "../../store/sessionSlice";
 
-import { Navbar, RowNav, List, Item, RightInfo, SessionActions } from "./styles";
+import { Aside, Navbar, Menu, RowNav, List, Item, RightInfo, SessionActions } from "./styles";
 import Container from "../../shared/Container";
 import Logo from "../../shared/Logo";
 
+import menuIcon from "../../assets/menu.svg";
 import avatarIcon from "../../assets/avatar.svg";
+import closeIcon from "../../assets/close.svg";
 
 export default function Nav(): ReactElement {
+    const [openAsideNav, setOpenAsideNav] = useState<boolean>(false)
     const { pathname } = useLocation();
     const dispatch = useDispatch();
 
@@ -19,7 +23,7 @@ export default function Nav(): ReactElement {
 
         return <Navigate to="/signin" />
     }
-
+    
     return (
         <Navbar>
             <Container>
@@ -50,8 +54,41 @@ export default function Nav(): ReactElement {
                             </span>
                         </SessionActions>
                     </RightInfo>
+                    <Menu src={menuIcon} alt="Menu" onClick={() => setOpenAsideNav(true)} />
+                    {openAsideNav && <AsideNav logout={logout} pathname={pathname} onClose={() => setOpenAsideNav(false)} />}      
                 </RowNav>
             </Container>
         </Navbar>
+    )
+}
+
+function AsideNav({logout, onClose, pathname}: {logout: VoidFunction, onClose: VoidFunction, pathname: string}): ReactElement {
+    return(
+        <Aside>
+            <SessionActions mobile={true}>
+                <img src={avatarIcon} alt="Eu" />
+                <span onClick={() => logout()}>
+                    Sair
+                </span>
+                <img src={closeIcon} alt="Fechar" onClick={onClose} />
+            </SessionActions>
+            <List mobile={true}>
+                <Item mobile={true}>
+                    <Link to="/characters">
+                        <span className={pathname == "/characters" ? "active" : ""}>Personagens</span>    
+                    </Link>
+                </Item>
+                <Item  mobile={true}>
+                    <Link to="/movies">
+                        <span className={pathname == "/movies" ? "active" : ""}>Filmes</span>    
+                    </Link>
+                </Item>
+                <Item  mobile={true}>
+                    <Link to="/hqs">
+                        <span className={pathname == "/hqs" ? "active" : ""}>HQs</span>    
+                    </Link>
+                </Item>
+            </List>
+        </Aside>
     )
 }
--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -1,8 +1,47 @@
-import styled, { keyframes } from 'styled-components';
+import styled, {keyframes} from 'styled-components';
+
+const openNavBar = keyframes`
+    0% {
+        width: 0;
+        opacity: 0;
+    }
+    100% {
+        width: 100%;
+        opacity: 1;
+    }
+`
+    
+export const Aside = styled.aside`
+    height: 100%;
+    position: fixed;
+    z-index: 1;
+    width: 100%;
+    top: 0; 
+    right: 0;
+    background-color: #ff0000;
+    overflow-x: hidden;
+    padding-top: 10px;
+    transition: 0.5s;
+    animation: ${openNavBar} 0.3s ease;
+`;
 
 export const Navbar = styled.nav`
     background-color: var(--background-color-nav);
     height: 114px;
+
+    @media (max-width: 720px) {
+        padding: 0px 15px;
+    }
+`;
+
+export const Menu = styled.img<any>`
+    display: none;
+    height: 58px;
+    cursor: pointer;
+
+    @media (max-width: 720px) {
+        display: block;
+    }
 `;
 
 export const RowNav = styled.div`
@@ -12,18 +51,23 @@ export const RowNav = styled.div`
     height: 114px;
 `;
 
-export const List = styled.ul`
+export const List = styled.ul<any>`
     display: flex;
     align-items: center;
+    flex-direction: ${(p: any) => p.mobile ? "column" : "row"};
 `
 
-export const Item = styled.li`
+export const Item = styled.li<any>`
+    border-bottom: ${(p: any) => p.mobile ? "1px solid white" : null};
+    width: 100%;
+    text-align: center;
+
     span {
         font-family: Axiform;
         color: #84848D;
         font-size: 30px;
         transition: color 0.1s ease-in;
-        margin-right: 85px;
+        margin-right: ${(p: any) => p.mobile ? null : "85px"};
         cursor: pointer;
 
         &:hover, &.active {
@@ -37,14 +81,18 @@ export const RightInfo = styled.div`
     justify-content: space-between;
     align-items: center;
     height: 114px;
+
+    @media (max-width: 720px) {
+        display: none;
+    }
 `;
 
-export const SessionActions = styled.div`
+export const SessionActions = styled.div<any>`
     display: flex;
     justify-content: space-between;
     align-items: center;
     height: 114px;
-
+    
     span {
         font-family: Axiform Light;
         color: #84848D;
@@ -54,5 +102,9 @@ export const SessionActions = styled.div`
         &:hover {
             color: #ffffff; 
         }
+    }
+
+    @media (max-width: 720px) {
+        padding: 0px 15px;
     }
 `;
